030a1dee16dd0828c23108806d45dd94ca723ce0
mohdsams23 <157204613+Mohdsams023@users.noreply.github.com>
2024-02-03 19:10:46 +0530

    complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+// Header and footer added dynamycally
 $(function () {
     $('#header').load('includes/header.html');
     $('#footer').load('includes/footer.html');
+});
+
+// login Validation 
+function validateLogin() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    if (email === "admin@admin.com" && password === "123456") {
+      alert("Login successful!");
+    } else {
+      alert("Invalid credentials. Please try again.");
+    }
+  }
+
+  // Header and footer added All Pages dynamycally
+$(function () {
+  $('#header').load('../includes/header.html');
+  $('#footer').load('../includes/footer.html');
 });
