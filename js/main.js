d7202168402990c7b3aa0ef34e52fb06bd0f4315
Mohd Samsuddin <157204613+Mohdsams023@users.noreply.github.com>
2024-02-03 20:18:36 +0530

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 // Header and footer added dynamycally
-$(function () {
-    $('#header').load('./includes/header.html');
-    $('#footer').load('./includes/footer.html');
-});
+
 
 // login Validation 
 function validateLogin() {
